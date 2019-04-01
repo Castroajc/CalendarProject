654c044b2bfa7cc6740e10aa5164f9abbcb4d317
Castroajc <datmexicanbuy@gmail.com>
2019-03-31 20:54:56 -0400

    Added in a different project for the database. The index razorpage is now showing the static html page

---
+++ CalendarDotNet/CalendarDatabase/days.sql
@@ -0,0 +1,17 @@
+﻿/*
+The database must have a MEMORY_OPTIMIZED_DATA filegroup
+before the memory optimized object can be created.
+
+The bucket count should be set to about two times the 
+maximum expected number of distinct values in the 
+index key, rounded up to the nearest power of two.
+*/
+
+CREATE TABLE [dbo].[Table1]
+(
+	[monthId] INT NOT NULL PRIMARY KEY NONCLUSTERED HASH WITH (BUCKET_COUNT = 131072), 
+    [eventDescription] VARCHAR(50) NULL, 
+    [eventTitle] VARCHAR(50) NULL, 
+    [startTime] INT NULL, 
+    [dayOfWeek] INT NULL
+) WITH (MEMORY_OPTIMIZED = ON)
---
+++ CalendarDotNet/CalendarDatabase/months.sql
@@ -0,0 +1,16 @@
+﻿/*
+The database must have a MEMORY_OPTIMIZED_DATA filegroup
+before the memory optimized object can be created.
+
+The bucket count should be set to about two times the 
+maximum expected number of distinct values in the 
+index key, rounded up to the nearest power of two.
+*/
+
+CREATE TABLE [dbo].[Table2]
+(
+	[monthId] INT NOT NULL PRIMARY KEY NONCLUSTERED HASH WITH (BUCKET_COUNT = 131072), 
+    [year] INT NULL, 
+    [calendarDate] VARCHAR(50) NULL, 
+    [monthName] CHAR(10) NULL
+) WITH (MEMORY_OPTIMIZED = ON)
